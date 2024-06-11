b584ccf7682e0e511c1a8d872685e10434ec788e
PM <paolo.miccoli.83@gmail.com>
2024-06-11 13:34:47 +0200

    log time to 3 sec and 60 samples

--- src/components/TestChart.js
+++ src/components/TestChart.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Line } from "react-chartjs-2";
 import 'chart.js/auto';
 import { extractNodeIdAndValue, findVariableByNodeId } from "../function/variableUtils";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const initialData = {
   labels: [], // Inizializza labels come array vuoto
@@ -27,10 +28,10 @@ const options = {
 };
 
 const TestChart = ({ variables }) => {
-    const [data, setData] = useState(() => {
-        const savedData = localStorage.getItem('chartData');
-        return savedData ? JSON.parse(savedData) : initialData;
-      });
+  const [data, setData] = useState(() => {
+    const savedData = localStorage.getItem('chartData');
+    return savedData ? JSON.parse(savedData) : initialData;
+  });
 
   useEffect(() => {
     const insNewValLogging = () => {
@@ -41,36 +42,62 @@ const TestChart = ({ variables }) => {
         const currentDate = new Date().toLocaleString(); // Ottieni la data e l'ora corrente
         
         setData(prevData => {
-            const updatedData = {
-              ...prevData,
-              labels: [...(prevData.labels || []), currentDate], // Utilizza un array vuoto se prevData.labels non è definito
-              datasets: [
-                {
-                  ...prevData.datasets[0],
-                  data: [...prevData.datasets[0].data, newVal1].slice(-60) // Limita a 60 valori
-                },
-                {
-                  ...prevData.datasets[1],
-                  data: [...prevData.datasets[1].data, newVal2].slice(-60) // Limita a 60 valori
-                }
-              ]
-            };
-  
-            localStorage.setItem('chartData', JSON.stringify(updatedData));
-            return updatedData;
-          });
+          const newLabels = [...prevData.labels, currentDate];
+          const newData1 = [...prevData.datasets[0].data, newVal1];
+          const newData2 = [...prevData.datasets[1].data, newVal2];
+
+          // Se ci sono più di 60 punti, rimuovi il primo punto
+          if (newLabels.length > 60) {
+            newLabels.shift();
+            newData1.shift();
+            newData2.shift();
+          }
+
+          const updatedData = {
+            ...prevData,
+            labels: newLabels,
+            datasets: [
+              {
+                ...prevData.datasets[0],
+                data: newData1
+              },
+              {
+                ...prevData.datasets[1],
+                data: newData2
+              }
+            ]
+          };
+
+          localStorage.setItem('chartData', JSON.stringify(updatedData));
+          return updatedData;
+        });
 
       } catch (error) {
         console.error(error);
       }
     };
-    const intervalId = setInterval(insNewValLogging, 5000);
+
+    const intervalId = setInterval(insNewValLogging, 1000);
     return () => clearInterval(intervalId);
   }, [variables]);
+
+  const resetCache = () => {
+    try {
+      window.localStorage.clear();
+      setData(initialData); // Usa initialData direttamente
+    } catch (error) {
+      console.error('Error resetting cache:', error);
+    }
+  };
+
 
   return (
     <div className="chart-container" style={{ position: "relative", width: "100%", height: "400px" }}>
       <Line data={data} options={options} />
+      <section>
+      <button  type="button" className="btn btn-primary" onClick={resetCache} >Reset Chart</button>
+      </section>
+      
     </div>
   );
 }
